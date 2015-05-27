7b198a1e7402bc95756c0d51a4b1841c004d3663
spacegeek224 <marisusisme@gmail.com>
2015-05-27 12:00:31 -0400

    asdf

--- src/main/java/io/github/spacegeek224/CodeCrackerMain.java
+++ src/main/java/io/github/spacegeek224/CodeCrackerMain.java
@@ -9,17 +9,35 @@ public class CodeCrackerMain {
 		
 		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
-		String uword = input.nextLine();
-		int uwordl = uword.length();
-		outln("Word: " + uword);
-		outln("Length: " + Integer.toString(uwordl));
+		String word = input.nextLine();
+		final int wordl = word.length();
+		outln("Word: " + word);
+		outln("Length: " + Integer.toString(wordl));
+		//Generate arrays
+		char[] alpha = new char[26];
+		int k = 0;
+		for (int i = 0; i < 26; i++) {
+			alpha[i] = (char)(97 + (k++));
+		}
+		char[] wordc = new char[wordl];
+		for (int i = 0; i < wordl; i++) {
+			wordc[i] = word.charAt(i);
+		}
 		
+		//Print out individual characters
+		for (int i = 0; i < wordl; i++) {
+		}
 	}
+
 	public static void outln(String s) {
 		System.out.println(s);
 	}
 	public static void out(String s) {
 		System.out.print(s);
 	}
-
+		// TODO Replacing method
+		/**
+		String test = "hello";
+		test.replace(oldChar, newChar);
+		 */
 }
